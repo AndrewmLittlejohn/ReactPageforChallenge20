0d5f62778f2331e25f34e457d77c2a9612e91da8
Andrew Littlejohn <andrewlittlejohn@andrews-air.myfiosgateway.com>
2023-04-11 09:57:47 -0400

    getting close

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light" id='NavBarFull'>
       <div className="container">
         <Link className="navbar-brand" to="/">Andrew's React App</Link>
-        // nav button still doesn't work 
-        <button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarContent" type="button">
+           <button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" type="button">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarContent">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav" id="NavBarList">
             <li className="nav-item">
               <Link className="nav-link" to="/about">About Me</Link>
--- src/components/resume.js
+++ src/components/resume.js
@@ -2,12 +2,18 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Resume = () => {
+
+  const LineBreaks = 4;
+  const whiteSpace = Array(LineBreaks).fill(null).map((_, index) => <br key={index} />);
+
   return (
     <div className="container">
       <h1>My Resume</h1>
       <p>
-        <a href="AndrewL_resume.pdf" download="Andrew_Littlejohn_Resume_April_2023.pdf">
-          Click here to download my resume</a>
+        <a href="./AndrewL_resume.pdf" target="_blank" rel="noopener noreferrer" download="Andrew_Littlejohn_Resume_April_2023.pdf">
+          Download my resume</a> 
+          {whiteSpace}
+          As of challenge submittal the resume is still in the updating process, it is currently a minimum viable product, stress on minimum
       </p>
     </div>
   );

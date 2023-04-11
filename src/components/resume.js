c02187b96b894f3d68968ad059a0de4cc8a8c79d
Andrew Littlejohn <andrewlittlejohn@andrews-air.myfiosgateway.com>
2023-04-11 14:45:06 -0400

    updated pdf path

--- src/components/resume.js
+++ src/components/resume.js
@@ -9,7 +9,7 @@ const Resume = () => {
     <div className="container">
       <h1>My Resume</h1>
       <p>
-        <a href="./AndrewL_resume.pdf" target="_blank" rel="noopener noreferrer" download="Andrew_Littlejohn_Resume_April_2023.pdf">
+        <a href="/AndrewL_resume.pdf" target="_blank" rel="noopener noreferrer" download="Andrew_Littlejohn_Resume_April_2023.pdf">
           Download my resume</a> 
           {whiteSpace}
           As of challenge submittal the resume is still in the updating process, it is currently a minimum viable product, stress on minimum
